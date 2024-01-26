e70cb7cb1a92e5e8730c17f234223c9fd3a367ae
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2024-01-26 02:02:29 -0500

    updated schema

--- vortex/dagster/schema.sql
+++ vortex/dagster/schema.sql
@@ -25,3 +25,20 @@ options (
   table_id 'tblOXn0lWpf4gTjFz',
   rowid_column 'article_id'
 );
+
+
+create table public.articles_summary (
+    summary_id serial primary key,
+    summary text not null,
+    dependencies json not null,
+    created_at timestamp not null default now()
+);
+
+create table public.processed_articles (
+    article_id int primary key,
+    url text not null,
+    content text not null,
+    summary text not null,
+    created_at timestamp not null default now(),
+    reprocess boolean not null default true
+);
