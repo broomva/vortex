e6510f02129bfe4e002f1b1e7eba11a373651647
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2024-02-09 18:11:05 -0500

    chore: updated link in home page and port in package.json for vortex flows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Check the Vortex API at&nbsp;
-          <Link href="/api/check">
+          <Link href="https://vortex-api.broomva.tech/">
             <code className="font-mono font-bold">api/check</code>
           </Link>
         </p>
