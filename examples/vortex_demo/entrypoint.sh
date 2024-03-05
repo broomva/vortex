c657986c2306f68ba188bae83a4cc4774627b67a
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2024-03-05 00:53:59 -0500

    fix: testing manually defined gprc server

--- examples/vortex_demo/entrypoint.sh
+++ examples/vortex_demo/entrypoint.sh
@@ -6,5 +6,8 @@ dagster-webserver -h 0.0.0.0 -p 3000 &
 # Start the second process
 dagster-daemon run &
 
+# Start the third process
+dagster api grpc --python-file -dags/web_rag_agent.py --host 0.0.0.0 --port 4266 &
+
 # Wait for both processes to finish
 wait -n
